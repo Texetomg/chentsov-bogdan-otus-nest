61095134885e54fc0c13b567ca7ab2d193a4a2c3
texetomg <texetomg@gmail.com>
2024-03-31 09:50:42 +0300

    Genereated resources.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
