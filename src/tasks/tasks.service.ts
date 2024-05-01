989eed2f51f9835a986c279b3f220e5445760083
texetomg <texetomg@gmail.com>
2024-05-01 16:58:21 +0300

    Init libs + modules.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { EDifficulty } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
   private tasks = [
     {
       id: 1,
-      name: 'Biba',
+      name: '1137. N-th Tribonacci Number',
       description: 'description',
+      difficulty: EDifficulty.EASY,
     },
     {
       id: 2,
-      name: 'Boba',
+      name: 'Two Sum',
       description: 'description',
+      difficulty: EDifficulty.EASY,
     },
   ];
 
